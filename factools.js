ed19875cbd178fc1d2bc867dc438675c7cc98964
Marcelo <m_tonho@yahoo.com.br>
2015-07-09 15:22:53 -0300

    Helpers work in production + extra styling

--- factools.js
+++ factools.js
@@ -1,16 +1,3 @@
-var langs = [
-      {name: "MeteorJS", imageName: "meteor"},
-      {name: "NodeJS", imageName: "nodejs"},
-      {name: "AngularJS", imageName: "angularjs"},
-      {name: "JavaScript", imageName: "javascript"},
-      {name: "Ruby/Rails", imageName: "ruby"}
-  ];
-var types = [
-      {name: "Post/Tutorial", colorClass: "post"},
-      {name: "Library/Documentation", colorClass: "lib"},
-      {name: "Services", colorClass: "service"}
-];
-
 if (Meteor.isClient) {
   Session.setDefault("selectedLanguage", "All");
   Session.setDefault("selectedType", "All");
@@ -87,53 +74,9 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
   });
 }
-
-UI.registerHelper('langImage', function(options) {
-  var name = options.hash.name;
-
-  for(var i = 0; i < langs.length; i++) {
-    if (name == langs[i].name) {
-      return '/img/' + langs[i].imageName + '-icon.png';
-    }
-  } 
-});
-
-UI.registerHelper('isSelectedType', function(options) {
-  var name = options.hash.name;
-  var selectedType = Session.get("selectedType");
-
-  if (name == selectedType) {
-    return 'selected';
-  } else {
-    return 'not-selected';
-  }
-
-});
-
-UI.registerHelper('isSelectedLanguage', function(options) {
-  var name = options.hash.name;
-  var selectedLanguage = Session.get("selectedLanguage");
-
-  if (name == selectedLanguage) {
-    return 'selected';
-  } else {
-    return 'not-selected';
-  }
-
-});
-
-UI.registerHelper('typeColor', function(options) {
-  var type = options.hash.type;
-
-  for(var i = 0; i < types.length; i++) {
-    if (type == types[i].name) {
-      return types[i].colorClass;
-    }
-  } 
-});
 
 Meteor.methods({
   addEntry: function (entry) {
